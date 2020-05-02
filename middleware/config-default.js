036a65c8e89b9927ee4d8455a2aa28a8840e3096
Mr.King <1120427301@qq.com>
2020-05-02 16:13:55 +0800

    node商城

---
+++ middleware/config-default.js
@@ -0,0 +1,12 @@
+
+var config = {
+    dialect: 'mysql',
+    database: 'init-sql',
+    username: 'root',
+    password: 'root',
+    host: 'localhost',
+    port: 3306,
+    rootpath: '/zshop'
+};
+
+module.exports = config;
